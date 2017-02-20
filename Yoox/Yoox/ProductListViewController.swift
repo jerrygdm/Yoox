503e4db910c0aeeb99809d5be9ea850058637747
Gianmaria Dal Maistro <gianmaria.dalmaistro@icloud.com>
2017-02-20 11:41:39 +0100

    implemented reactive way to load new data and add to collection view

--- Yoox/Yoox/ProductListViewController.swift
+++ Yoox/Yoox/ProductListViewController.swift
@@ -7,16 +7,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SDWebImage
 
 class ProductListViewController: UIViewController
 {
-
-    override func viewDidLoad() {
+    var productListViewModel = ProductListViewModel()
+    let disposeBag = DisposeBag()
+    
+    var items = [Product]()
+    var numCellsPerRow = 3
+    
+    @IBOutlet weak var collectionView : UICollectionView?
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.collectionView?.rx_nextPageTrigger.map{ offset  in
+            return offset
+            }.scan(0){ lastCount, newValue in
+                return lastCount + 1
+            }.flatMapLatest{ page in
+                return self.productListViewModel.getProducts(String(page))
+            }.subscribeNext{ response in
+                if let products = response?.items
+                {
+                    let range = NSMakeRange(self.items.count, products.count)
+                    let newIndexes : NSIndexSet = NSIndexSet(indexesInRange: range)
+                    
+                    var newIndexPath = [NSIndexPath]()
+                    
+                    newIndexes.enumerateIndexesUsingBlock({ (idx, stop) in
+                        newIndexPath.append((NSIndexPath(forRow: idx, inSection: 0)))
+                    })
+                    
+                    self.items.appendContentsOf(products)
+                    
+                    self.collectionView?.performBatchUpdates({
+                        self.collectionView?.insertItemsAtIndexPaths(newIndexPath)
+                        }, completion: { (completion) in
+                    })
+                }
+            }.addDisposableTo(disposeBag)
     }
-    
-    
 }
 
 extension UICollectionView
