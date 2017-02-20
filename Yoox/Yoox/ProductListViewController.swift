516b5c38a8444fb63e02510bce92bb7ab7eadcf7
Gianmaria Dal Maistro <gianmaria.dalmaistro@icloud.com>
2017-02-20 11:56:18 +0100

    updated project

--- Yoox/Yoox/ProductListViewController.swift
+++ Yoox/Yoox/ProductListViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Yoox
 //
-//  Created by Gianmaria Dal Maistro on 20/02/17.
-//  Copyright © 2017 it.whiteworld.yoox. All rights reserved.
+//  Created by Gianmaria Dal Maistro on 17/02/17.
+//  Copyright © 2017 it.whiteworld.test. All rights reserved.
 //
 
 import UIKit
@@ -15,7 +15,7 @@ class ProductListViewController: UIViewController
 {
     var productListViewModel = ProductListViewModel()
     let disposeBag = DisposeBag()
-    
+
     var items = [Product]()
     var numCellsPerRow = 3
     
@@ -24,33 +24,70 @@ class ProductListViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+
         self.collectionView?.rx_nextPageTrigger.map{ offset  in
             return offset
-            }.scan(0){ lastCount, newValue in
-                return lastCount + 1
-            }.flatMapLatest{ page in
-                return self.productListViewModel.getProducts(String(page))
-            }.subscribeNext{ response in
-                if let products = response?.items
-                {
-                    let range = NSMakeRange(self.items.count, products.count)
-                    let newIndexes : NSIndexSet = NSIndexSet(indexesInRange: range)
-                    
-                    var newIndexPath = [NSIndexPath]()
-                    
-                    newIndexes.enumerateIndexesUsingBlock({ (idx, stop) in
-                        newIndexPath.append((NSIndexPath(forRow: idx, inSection: 0)))
-                    })
-                    
-                    self.items.appendContentsOf(products)
-                    
-                    self.collectionView?.performBatchUpdates({
-                        self.collectionView?.insertItemsAtIndexPaths(newIndexPath)
-                        }, completion: { (completion) in
-                    })
-                }
-            }.addDisposableTo(disposeBag)
+        }.scan(0){ lastCount, newValue in
+            return lastCount + 1
+        }.flatMapLatest{ page in
+            return self.productListViewModel.getProducts(String(page))
+        }.subscribeNext{ response in
+            if let products = response?.items
+            {
+                let range = NSMakeRange(self.items.count, products.count)
+                let newIndexes : NSIndexSet = NSIndexSet(indexesInRange: range)
+                
+                var newIndexPath = [NSIndexPath]()
+                
+                newIndexes.enumerateIndexesUsingBlock({ (idx, stop) in
+                    newIndexPath.append((NSIndexPath(forRow: idx, inSection: 0)))
+                })
+                
+                self.items.appendContentsOf(products)
+                
+                self.collectionView?.performBatchUpdates({ 
+                    self.collectionView?.insertItemsAtIndexPaths(newIndexPath)
+                    }, completion: { (completion) in
+                })
+            }
+        }.addDisposableTo(disposeBag)
+    }
+}
+
+extension ProductListViewController : UICollectionViewDataSource
+{
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return self.items.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ProductCellID", forIndexPath: indexPath)
+        
+        if let cell = cell as? ProductListCell
+        {
+            let model = self.items[indexPath.row]
+            
+            cell.titleLabel?.text = model.title
+            cell.priceLabel?.text = model.priceString
+            cell.imageView?.sd_setImageWithURL(NSURL(string: model.imageUrlString))
+        }
+
+        return cell
+    }
+}
+
+extension ProductListViewController : UICollectionViewDelegate
+{
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        guard let flowLayout : UICollectionViewFlowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return CGSizeMake(100, 100)}
+
+        let availableWidthForCells = CGFloat(CGRectGetWidth(collectionView.frame) - flowLayout.sectionInset.left - flowLayout.sectionInset.right) - CGFloat(flowLayout.minimumInteritemSpacing) * CGFloat(numCellsPerRow - 1);
+        let cellWidth = availableWidthForCells / CGFloat(numCellsPerRow)
+        
+        return CGSizeMake(cellWidth, flowLayout.itemSize.height);
     }
 }
 
---
+++ Yoox/Yoox/Views/Cells/ProductListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListCell.swift
+//  Yoox
+//
+//  Created by Gianmaria Dal Maistro on 19/02/17.
+//  Copyright © 2017 it.whiteworld.test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProductListCell : UICollectionViewCell
+{
+    @IBOutlet weak var titleLabel : UILabel?
+    @IBOutlet weak var priceLabel : UILabel?
+    @IBOutlet weak var imageView : UIImageView?
+}
